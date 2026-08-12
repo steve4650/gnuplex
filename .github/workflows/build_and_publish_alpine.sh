4f7e93e68691c161f68ec968c71d07b4aeb520f2
steve <pub@davisgroup.uk>
2026-08-11 21:29:40 -0500

    Fix build script and add more logging

--- .github/workflows/build_and_publish_alpine.sh
+++ .github/workflows/build_and_publish_alpine.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+set -x
 
 umask 077
 echo "$1" >/tmp/gnuplex-deploy
@@ -8,7 +9,7 @@ GNUPLEX_GO_VERSION="$2"
 apk add uv git curl bash openssh gcc musl-dev libstdc++ unzip
 
 cd /usr/local/bin
-wget "https://go.dev/dl/go$GNUPLEX_GO_VERSION.linux-amd64.tar.gz"
+wget "https://go.dev/dl/$GNUPLEX_GO_VERSION.linux-amd64.tar.gz"
 tar xvzf go*.gz
 mv go godir
 ln -s godir/bin/go .
